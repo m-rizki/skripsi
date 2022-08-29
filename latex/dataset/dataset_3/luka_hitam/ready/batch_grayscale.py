892745c3d1e454ddffc5b58e648f4b82a3d3c5a5
m-rizki <mrizki270798@gmail.com>
2022-08-29 07:34:29 +0700

    added latex/ pdf/ ppt/

---
+++ latex/dataset/dataset_3/luka_hitam/ready/batch_grayscale.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.io import imread
+from skimage.color import rgb2gray
+
+
+data_hitam = ["1", "5", "15", "26", "29"]
+
+data = data_hitam
+
+my_dpi = 96 # https://www.infobyip.com/detectmonitordpi.php
+
+for i in data:
+    
+    filename = str(i)
+
+    img = imread(filename + ".jpg")
+    img = rgb2gray(img)
+
+    # plot grayscale
+    fig= plt.figure(frameon=False, figsize=(img.shape[1]/my_dpi, img.shape[0]/my_dpi), dpi=my_dpi)
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.imshow(img, cmap=plt.cm.gray)
+    ax.set_xticks([]), ax.set_yticks([]) # hide axes
+    ax.axis('off')
+    
+    outname = filename + "_gray" + ".jpg"
+
+    plt.savefig(outname, dpi=my_dpi)
+    print(outname + " has been saved")
+    plt.close(fig)
---
+++ latex/dataset/dataset_3/readme.txt
@@ -0,0 +1,3 @@
+https://stackoverflow.com/questions/21884271/warning-about-too-many-open-figures
+warning about too many open figures
+-> Use .clf or .cla on your figure object instead of creating a new figure. From @DavidZwicker
